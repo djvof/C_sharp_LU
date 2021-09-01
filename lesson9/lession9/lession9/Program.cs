4ff92d245112bd1902b91aa8bfa2a786a6e11925
Vitaly <vit@bb-tech.eu>
2021-09-01 22:18:13 +0300

    gfgggg

--- lesson9/lession9/lession9/Program.cs
+++ lesson9/lession9/lession9/Program.cs
@@ -6,7 +6,7 @@ namespace lession9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");   
         }
     }
 }
