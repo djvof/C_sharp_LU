ba70561e96fda5fd31b73c03b7ba12181e0f4439
Vitaly <vit@bb-tech.eu>
2021-09-08 21:56:43 +0300

    firstfiles

---
+++ Lekcijas2_MajasDarbs/Lekcijas2_MajasDarbs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lekcijas2_MajasDarbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
